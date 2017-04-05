028bac678c73b697c39532bcce05b99a29057367
pdarde-daitan <pdarde@daitangroup.com>
2017-04-05 17:11:23 -0300

    fix eslint errors

--- src/main/webapp/components/SetupInstructions/template.jsx
+++ src/main/webapp/components/SetupInstructions/template.jsx
@@ -9,19 +9,19 @@ const Instructions = () => (
     <p>Copy the URL provided above (Example image provided below). You will need this later to
     configure the integration correctly.</p>
     <figure>
-      <img src={require('./img/zapier_settings_step1.png')} alt="Webhook configuration"/>
+      <img src={require('./img/zapier_settings_step1.png')} alt="Webhook configuration" />
     </figure>
     <h4>Step 2</h4>
-    <p>Go to <a href='https://zapier.com/'>Zapier</a> and press "Make a Zap." Follow the
+    <p>Go to <a href='https://zapier.com/'>Zapier</a> and press &quot;Make a Zap.&quot; Follow the
     instructions provided by Zapier to choose your trigger app.</p>
     <figure>
-      <img src={require('./img/zapier_settings_step2.png')} alt="Webhook configuration"/>
+      <img src={require('./img/zapier_settings_step2.png')} alt="Webhook configuration" />
     </figure>
     <h4>Step 3</h4>
     <p>Follow the steps provided by Zapier to setup the trigger app (This is an example using
     Trello).</p>
     <figure>
-      <img src={require('./img/zapier_settings_step3.png')} alt="Webhook configuration"/>
+      <img src={require('./img/zapier_settings_step3.png')} alt="Webhook configuration" />
     </figure>
     <h4>Step 4</h4>
     <p>Choose <strong>Symphony</strong> as your action app. Follow the steps on Zapier to finish
@@ -29,7 +29,7 @@ const Instructions = () => (
     <p>You will need the URL you copied from Symphony previously. Place this URL into the Webhook
     URL field.</p>
     <figure>
-      <img src={require('./img/zapier_settings_step4.png')} alt="Webhook configuration"/>
+      <img src={require('./img/zapier_settings_step4.png')} alt="Webhook configuration" />
     </figure>
     {/* End editing area */}
   </div>
